bd00cada5579bdde09f76c283a46642764564865
Chinedu Ebinim <enoch4real7@gmail.com>
2019-01-31 00:46:24 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.use((req, res, next) => {
+    const log = `${new Date().toString()}, "${req.method}", "${req.url}" \n`
+    console.log(log)
+    
+    fs.appendFileSync('server.log', log, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+    next()
+})
+app.use((req, res, next) => {
+    res.render('maint.hbs')
+})
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        user: "somebody",
+        pageTitle: 'Home',
+        currentYear: new Date().getFullYear()
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About',
+        currentYear: new Date().getFullYear()
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'unable to process request'
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`running`)
+})
